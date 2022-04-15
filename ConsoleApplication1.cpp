82afbbe86ce324bfe5189306de42bdf0414e0f49
MX <MX@DESKTOP-2IV00RC>
2022-04-15 18:32:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	std::system("color 16");
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t    **                                                  \n";
+	std::cout << "\t\t\t    **     **                                             \n";
+	std::cout << "\t\t\t           **                                            \n";
+	std::cout << "\t\t\t              **                                          \n";
+	std::cout << "\t\t\t              **                                        \n\n";
+	std::cout << "\t\t\t                   **                  **                  \n";
+	std::cout << "\t\t\t                   **                  **                \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                      \n";
+	std::cout << "\t\t\t                     **                                  \n";
+	std::cout << "\t\t\t                     **              **                 \n";
+	std::cout << "\t\t\t                                     **                 \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                       \n";
+	std::cout << "\t\t\t                                                       \n";
+}	
